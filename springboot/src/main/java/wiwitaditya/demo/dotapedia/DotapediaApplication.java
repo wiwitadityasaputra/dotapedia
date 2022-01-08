a5e5c5528fb240730d8d5a1f57bed36eb47ee28f
Wiwit Aditya <wiwit.aditya.saputra@gmail.com>
2022-01-08 07:31:55 +0700

    Adding springboot

---
+++ springboot/src/main/java/wiwitaditya/demo/dotapedia/DotapediaApplication.java
@@ -0,0 +1,13 @@
+package wiwitaditya.demo.dotapedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotapediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotapediaApplication.class, args);
+	}
+
+}
---
+++ springboot/src/test/java/wiwitaditya/demo/dotapedia/DotapediaApplicationTests.java
@@ -0,0 +1,13 @@
+package wiwitaditya.demo.dotapedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotapediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
