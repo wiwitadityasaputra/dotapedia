8e8e426adc59547e1b1a7dfdf40737932347e02d
Wiwit Aditya <wiwit.aditya.saputra@gmail.com>
2023-03-18 23:53:07 +0700

    Fix missing angular path

--- springboot/src/main/java/wiwitaditya/demo/dotapedia/config/WebConfig.java
+++ springboot/src/main/java/wiwitaditya/demo/dotapedia/config/WebConfig.java
@@ -11,4 +11,15 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**");
     }
 
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("forward:/index.html");
+        registry.addViewController("/tournament").setViewName("forward:/index.html");
+        registry.addViewController("/team").setViewName("forward:/index.html");
+        registry.addViewController("/player").setViewName("forward:/index.html");
+        registry.addViewController("/hero").setViewName("forward:/index.html");
+        registry.addViewController("/schedule").setViewName("forward:/index.html");
+        registry.addViewController("/news").setViewName("forward:/index.html");
+    }
+
 }
