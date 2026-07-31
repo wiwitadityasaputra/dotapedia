6818d6b3ae788fd8f5ba09b8c72427b59252e3a6
Wiwit Aditya <wiwit.aditya.saputra@gmail.com>
2026-08-01 00:24:36 +0700

    Remove withXhr

--- src/angular/src/app/app.module.ts
+++ src/angular/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { provideHttpClient, withInterceptorsFromDi, withXhr } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -74,6 +74,6 @@ import { NewsComponent } from './news/news.component';
         SlideShowService,
         HomeService,
         NewsService,
-        provideHttpClient(withXhr(), withInterceptorsFromDi()),
+        provideHttpClient(withInterceptorsFromDi()),
     ] })
 export class AppModule { }
